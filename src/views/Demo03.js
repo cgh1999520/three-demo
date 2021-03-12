df0d40fa3ee5fd291addfd367b0fd4e5940ccb5b
root <cgh1999520@gmail.com>
2021-03-12 17:38:19 +0800

    update: 优化布局。

--- src/views/Demo03.js
+++ src/views/Demo03.js
@@ -9,12 +9,27 @@ export default class Demo03 extends React.Component {
         super(props);
         this.threeStage = React.createRef()
     }
+
     componentDidMount() {
         const threeStage = this.threeStage.current;
         // 加载 obj 模型
         const objLoader = new OBJLoader2();
         objLoader.load('/model/obj1/1.obj', (root) => {
             threeStage.scene.add(root);
+            // 白色聚光灯从侧面发光，投射阴影
+            var spotLight = new THREE.SpotLight("red");
+            spotLight.position.set(100, 1000, 100);
+
+            spotLight.castShadow = true;
+
+            spotLight.shadow.mapSize.width = 1024;
+            spotLight.shadow.mapSize.height = 1024;
+
+            spotLight.shadow.camera.near = 500;
+            spotLight.shadow.camera.far = 4000;
+            spotLight.shadow.camera.fov = 30;
+
+            threeStage.scene.add(spotLight);
         });
     }
 
